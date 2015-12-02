f76ba5c2333f5c9c8a8b161c0134ff2beb1eb11d
drecinos37 <drecinos@chicagobooth.edu>
2015-12-01 21:56:10 -0600

    Forgot to save

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,12 @@
 class Favorite < ActiveRecord::Base
+
+
+validates :user_id, :uniqueness => { :scope => :address }
+validates :venue_id, :uniqueness => { :scope => :address }
+validates :dish_id, :uniqueness => { :scope => :address }
+
+
+belongs_to :user
+belongs_to :venue
+belongs_to :dish
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,8 @@
 class Venue < ActiveRecord::Base
+validates :name, :presence => true, :uniqueness => true
+validates :neighborhood_id, :presence => true, :uniqueness => true
+
+belongs_to :neighborhood
+has_many :favorites
+
 end
